91449ff2114e90ce86fbeab783f1e2bb97a42916
John chukwu <ifeanyichukwujohn23@gmail.com>
2025-11-20 12:37:51 +0100

    removed api keys

--- firebase.ts
+++ firebase.ts
@@ -1,14 +1,21 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+import Constants from "expo-constants";
+
+if (!Constants.expoConfig?.extra) {
+  throw new Error("Missing Expo config 'extra' in app config (Constants.expoConfig.extra)");
+}
+const extra = Constants.expoConfig.extra;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBYUZKafAeIBqeX3251OAogN_lIpRUt0dU",
-  authDomain: "nysc-backend.firebaseapp.com",
-  projectId: "nysc-backend",
-  storageBucket: "nysc-backend.firebasestorage.app",
-  messagingSenderId: "538426365974",
-  appId: "1:538426365974:web:038145797e76891bbcfd37",
-  measurementId: "G-9GS2QV3FYD"
+  apiKey: extra.firebaseApiKey,
+  authDomain: extra.firebaseAuthDomain,
+  projectId: extra.firebaseProjectId,
+  storageBucket: extra.firebaseStorageBucket,
+  messagingSenderId: extra.firebaseMessagingSenderId,
+  appId: extra.firebaseAppId,
+  measurementId: extra.firebaseMeasurementId
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
